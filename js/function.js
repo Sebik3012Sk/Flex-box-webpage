e4c84462eec6bbc515d97c0185c256ac607c0ee9
Sebik3012Sk <sebik-kucera@hotmail.com>
2022-11-15 06:39:52 +0100

    Flex Box project 15.11.2022

--- js/function.js
+++ js/function.js
@@ -30,9 +30,11 @@ export function search(event){
     for(let d of list_destinatiaon){
         if(parse_data[0] == d){
             location.assign("https://www.blue-style.cz/first-minute/?gclid=Cj0KCQiAyMKbBhD1ARIsANs7rEHCzH0spc9PDTDlpPS5n7n855kTXLale1B_FhWVExSXrrjgvLx9lWQaAskJEALw_wcB")
+            break;
         }
-
     }
+
+    event.target.value = "";
 }
 
 export function addSearchListener(){
